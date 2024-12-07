868f574d7fde21505117d41b649f6babbe639e5c
Gunslingahhh <abgmhafizhan1908@gmail.com>
2024-12-07 11:58:17 +0800

    Fixing bid interface

--- php/jobboard_detail.php
+++ php/jobboard_detail.php
@@ -55,64 +55,95 @@ $userName=$_SESSION['username'];
     <!-- Main Content -->
     <main>
         <div class="employee-dashboard">
-            <div class="table gig-card">
-                    <p><b>Title: </b><?php echo($tasktitle) ?></p>
-                    <p><b>Description: </b><?php echo($taskdescription) ?></p>
-                    <p><b>Date: </b><?php echo($taskdate) ?></p>
-                    <p><b>Duration: </b><?php echo($taskduration) ?></p>
-                    <p><b>Location: </b><?php echo($tasklocation) ?></p>
-                    <p><b>Tools Required: </b><?php echo($toolsrequired) ?></p>
-                    <p><b>Pax: </b><?php echo($pax) ?></p>
-                    <p><b>Price: RM</b><?php echo($price) ?></p>
-                    <p><b>Dress code: </b><?php echo($dresscode) ?></p>
-                    <p><b>Gender: </b><?php echo($gender) ?></p>
-                    <p><b>Nationality: </b><?php echo($nationality) ?></p>
-                    <p><b>Age range: </b><?php echo($agerange) ?></p>
-                    <p><b>Muslim friendly?: </b><?php echo($muslimfriendly) ?></p>
-                    <p><b>Food provided?: </b><?php echo($foodProvision) ?></p>
-                    <p><b>Transport provided?: </b><?php echo($transportProvision) ?></p>
-                    <p><b>User id: </b><?php echo($userid) ?></p>
-                    <table class="table table-striped">
-                        <thead class="thead">
-                            <tr>
-                                <th scope="col" class="col-2">Profile Picture</th>
-                                <th scope="col">Full Name</th>
-                                <th scope="col">Amount bid</th>
-                            </tr>
-                        </thead>
-                    </table>
-                    <?php
-                        if ($_SESSION['user_id'] != $userid){
-                            echo "
-                                <form>
-                                    <div class='row'>
-                                        <div class='col-sm-6'>
-                                            <div class='card text-center'>
-                                            <div class='card-body'>
-                                                <p class='card-title'>Starting bid</p>
-                                                <h5 class='card-text'>RM " . $price . "</h5>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div class='col-sm-6'>
-                                            <div class='card text-center'>
-                                            <div class='card-body'>
-                                                <p class='card-title'>Starting bid</p>
-                                                <h5 class='card-text'>Starting bid</h5>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p>Place your bid<input name='bidding_amount' type='number' class='form-control' min='" . $price . "' value='" . $price . "'></p>
-                                    <input class='btn btn-primary' type='submit' value='Place your bid'></input>
-                                </form>";
-                        }
-                        else{
-                            //Do nothing
-                        }
-                    ?>
-                </div>
+            <div class="gig-card">
+                <p><b>Title: </b><?php echo($tasktitle) ?></p>
+                <p><b>Description: </b><?php echo($taskdescription) ?></p>
+                <p><b>Date: </b><?php echo($taskdate) ?></p>
+                <p><b>Duration: </b><?php echo($taskduration) ?></p>
+                <p><b>Location: </b><?php echo($tasklocation) ?></p>
+                <p><b>Tools Required: </b><?php echo($toolsrequired) ?></p>
+                <p><b>Pax: </b><?php echo($pax) ?></p>
+                <p><b>Price: RM</b><?php echo($price) ?></p>
+                <p><b>Dress code: </b><?php echo($dresscode) ?></p>
+                <p><b>Gender: </b><?php echo($gender) ?></p>
+                <p><b>Nationality: </b><?php echo($nationality) ?></p>
+                <p><b>Age range: </b><?php echo($agerange) ?></p>
+                <p><b>Muslim friendly?: </b><?php echo($muslimfriendly) ?></p>
+                <p><b>Food provided?: </b><?php echo($foodProvision) ?></p>
+                <p><b>Transport provided?: </b><?php echo($transportProvision) ?></p>
+                <p><b>User id: </b><?php echo($userid) ?></p>
+                <table class="table table-striped">
+                    <thead class="thead">
+                        <tr>
+                            <th scope="col" class="col-2">Profile Picture</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">Amount bid</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
+                
+                            <?php
+                                if ($_SESSION['user_id'] != $userid) {
+                                    $sql = "SELECT u.user_photo, u.user_fullname, b.bidding_amount
+                                            FROM bidding b
+                                            INNER JOIN user u ON b.user_id = u.user_id
+                                            WHERE b.task_id = ?";
+
+                                    $stmt=$conn->prepare($sql);
+                                    $stmt->bind_param("i",$id);
+
+                                    if ($stmt->execute()) {
+                                        echo "
+                                        <div class='d-flex justify-content-center'>
+                    <div class='card w-75'>
+                        <div class='card-body'>
+                                    <form>
+                                        <div class='row d-flex justify-content-center'>
+                                            <div class='col-md-6 mb-3'>
+                                                <div class='card'>
+                                                    <div class='card-body text-center'>
+                                                        <h5 class='card-title'>Starting Bid</h5>
+                                                        <p class='card-text'>RM {$price}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class='col-md-6'>
+                                                <div class='card'>
+                                                    <div class='card-body text-center'>
+                                                        <h5 class='card-title'>Latest Bid</h5>
+                                                        <p class='card-text'>RM {$price}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class='col-md-6'>
+                                                <div class='card'>
+                                                    <div class='card-body text-center'>
+                                                        <h5 class='card-title'>Your Bid</h5>
+                                                        <div class='input-group mb-3 d-flex justify-content-center'>
+                                                            <span class='input-group-text'>RM</span>
+                                                            <input type='number' class='form-control-sm text-center' name='bidding_amount' min='{$price}' value='{$price}'>
+                                                        </div>
+                                                        <button type='submit' class='btn btn-primary'>Place Your Bid</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                        </div>
+                    </div>
+                </div>
+                                    ";
+                                        exit();
+                                    } else {
+                                        //do nothing
+                                    }
+
+                                    
+                                } else {
+                                    // Do nothing
+                                }
+                            ?>
         </div>
     </main>
 
--- php/topnav.php
+++ php/topnav.php
@@ -1,4 +1,4 @@
-<iv?php
+<?php
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
